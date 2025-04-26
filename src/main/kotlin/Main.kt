c80b014d02566bf2c40cf1623e55c956217e5308
Uros Karaleic <karaleicuu@gmail.com>
2025-04-26 18:11:21 +0200

    sa casa

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,13 +8,24 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
+data class Link(val url:String)
 
 @Composable
 @Preview
 fun App() {
     var text by remember { mutableStateOf("Hello, World!") }
+	var checked by remember{ mutableStateOf(false) }
+	
+	val links=listOf(
+		Link(url="link 1"),
+		Link(url="link 2"),
+		Link(url="link 3"),
+		Link(url="link 4"),
+		Link(url="link 5"),
+	)
 
     MaterialTheme {
+
 		Scaffold(
 			topBar={
 				TopAppBar(
@@ -23,7 +34,14 @@ fun App() {
 					}
 				)
 			},
-			content={padding->{}}
+			content={padding->
+				/*Switch(
+					checked=checked,
+					onCheckedChange={
+						checked=it
+					}
+				)*/
+			}
 		)
     }
 }
