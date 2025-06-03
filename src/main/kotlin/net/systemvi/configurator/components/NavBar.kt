54e37a22d7c9ebdbcd00ea707876c04238ec9f24
Jovan <colovicjovan02@gmail.com>
2025-06-03 13:27:02 +0200

    center buttons

--- src/main/kotlin/net/systemvi/configurator/components/NavBar.kt
+++ src/main/kotlin/net/systemvi/configurator/components/NavBar.kt
@@ -1,18 +1,24 @@
 package net.systemvi.configurator.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NavBar(){
     TopAppBar(
         title={
-            Row{
+            Row(modifier = Modifier.fillMaxSize(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically){
                 TextButton(
                     onClick = { println("Configure")}
                 ) {
@@ -27,6 +33,11 @@ fun NavBar(){
                     onClick = { println("Design")}
                 ) {
                     Text("Design")
+                }
+                TextButton(
+                    onClick = { println("Settings")}
+                ) {
+                    Text("Settings")
                 }
             }
 
