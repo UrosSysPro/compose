cfa6d1db19033ccde405df72cd7e4bae7887f071
Uros Karaleic <karaleicuu@gmail.com>
2025-06-06 16:19:51 +0200

    key125 idk

--- src/main/kotlin/net/systemvi/configurator/components/tester/TesterPage.kt
+++ src/main/kotlin/net/systemvi/configurator/components/tester/TesterPage.kt
@@ -16,16 +16,23 @@ import net.systemvi.configurator.components.tester.Grid
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ){
+        fun key125(name:String)="%$name,1.25,1.0".replace(" ","\\s")
+        fun key15(name:String)="%$name,1.5,1.0"
+        fun key175(name:String)="%$name,1.75,1.0"
+        val one="%,1.0,1.0"
+        val half="%,0.5,1.0"
+        val quarter="%,0.25,1.0"
+        val halfRow="%,1.0,0.25"
         Box() {
             Grid(
                 listOf(
-                    "Esc %,1.0,1.0 F1 F2 F3 F4 %,0.5,1.0 F5 F6 F7 F8 %,0.5,1.0 F9 F10 F11 F12 %,0.25,1.0 Print Scroll Pause %,0.25,1.0 Sleep Mute Vol- Vol+",
-                    "%,1.0,0.25",
-                    "` 1 2 3 4 5 6 7 8 9 0 - = %Backspace,2.0,1.0 %,0.25,1.0 Ins Home PgUp %,0.25,1.0 N.Lock / * -",
+                    "Esc $one F1 F2 F3 F4 $half F5 F6 F7 F8 $half F9 F10 F11 F12 $quarter Print Scroll Pause $quarter Sleep Mute Vol- Vol+",
+                    halfRow,
+                    "` 1 2 3 4 5 6 7 8 9 0 - = %Backspace,2.0,1.0 $quarter Ins Home PgUp $quarter N.Lock / * -",
                     "%Tab,1.5,1.0 q w e r t y u i o p [ ] %\\,1.5,1.0 %,0.25,1.0 Del End PgDn %,0.25,1.0 7 8 9 %+,1.0,2.0",
                     "%Caps\\sLock,1.75,1.0 a s d f g h j k l ; ' %Enter,2.25,1.0 %,3.5,1.0 4 5 6",
                     "%Left\\sShift,2.25,1.0 z x c v b n m , . / %Right\\sShift,2.75,1.0 %,1.25,1.0 ↑ %,1.25,1.0 1 2 3 %N.Enter,1.0,2.0",
-                    "%Left\\sCtrl,1.25,1.0 %Left\\sWin,1.25,1.0 %Left\\sAlt,1.25,1.0 %Space,6.25,1.0 %Right\\sAlt,1.25,1.0 %Right\\sWin,1.25,1.0 %Menu,1.25,1.0 %Right\\sCtrl,1.25,1.0 %,0.25,1.0 ← ↓ → %,0.25,1.0 %0,2.0,1.0 .",
+                    "${key125("Left Ctrl")} ${key125("Left Win")} ${key125("Left Alt")} %Space,6.25,1.0 ${key125("Right Alt")} ${key125("Right Win")} ${key125("Menu")} %Right\\sCtrl,1.25,1.0 %,0.25,1.0 ← ↓ → %,0.25,1.0 %0,2.0,1.0 .",
                 )
             )
         }
