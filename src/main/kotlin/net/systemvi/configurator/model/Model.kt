10b02e919e6dc1910d33823eabedf7f48c309b0a
Uros Karaleic <karaleicuu@gmail.com>
2025-06-09 17:39:54 +0200

    models

--- src/main/kotlin/net/systemvi/configurator/model/Model.kt
+++ src/main/kotlin/net/systemvi/configurator/model/Model.kt
@@ -1,9 +1,35 @@
 package net.systemvi.configurator.model
 
-enum class KeycapSize(val size:Float){
-    SIZE1U(1f)
+
+
+enum class KeycapWidth(val size:Float){
+    SIZE_1U(1f),
+    SIZE_125U(1.25f),
+    SIZE_15U(1f),
+    SIZE_175U(1f),
+    SIZE_2U(1f),
+    SIZE_225U(1f),
+    SIZE_275U(1f),
+    SIZE_625U(1f),
 }
+
+enum class KeycapHeight(val size:Float){
+    SIZE1U(1f),
+    SIZE2U(2f),
+}
+
+data class KeycapOffset(val x:Float,val y:Float)
+
 data class Key(val value:Byte, val name:String)
-data class Keycap(val key:Key)
-//data class Layout()
+
+data class Keycap(
+    val layers:List<Key>,
+    val width:KeycapWidth,
+    val height:KeycapHeight,
+    val offset: KeycapOffset,
+    val rotation:Float
+)
+
+data class Layout(val keycaps:List<List<>>)
+
 //data class Macro()
