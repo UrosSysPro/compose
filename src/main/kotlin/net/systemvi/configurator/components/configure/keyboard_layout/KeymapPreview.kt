af7408e40f9668913ca41467795e88750a75ce66
Uros Karaleic <karaleicuu@gmail.com>
2025-06-30 15:09:35 +0200

    nis

--- src/main/kotlin/net/systemvi/configurator/components/configure/keyboard_layout/KeymapPreview.kt
+++ src/main/kotlin/net/systemvi/configurator/components/configure/keyboard_layout/KeymapPreview.kt
@@ -1,13 +1,15 @@
 package net.systemvi.configurator.components.configure.keyboard_layout
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import arrow.core.Either
 import net.systemvi.configurator.components.configure.ConfigureViewModel
 import net.systemvi.configurator.model.KeyMap
 
@@ -15,19 +17,34 @@ import net.systemvi.configurator.model.KeyMap
     val viewModel= viewModel { ConfigureViewModel() }
     Card(
         modifier = Modifier
+            .padding(horizontal = 20.dp, vertical = 10.dp)
             .clickable(onClick = {viewModel.loadKeymap(keymap)})
+        ,
     ) {
        Column{
-           Text("Keymap name")
+           Text(keymap.name)
            Column{
                keymap.keycaps.forEach { row ->
                    Row{
                         row.forEach { key->
-                            val name=when(key.layers.first()){
-                                is Either.Left -> "macro"
-                                is Either.Right -> key.layers.first().getOrNull()!!.name
-                            }
-                            Text(name)
+                            //val name=when(key.layers.first()){
+                            //    is Either.Left -> "macro"
+                            //    is Either.Right -> key.layers.first().getOrNull()!!.name
+                            //}
+                            //Text(name)
+                            val size=14f
+                            val padding=4f
+                            val width=size*key.width.size
+                            val height=size
+                            Box(
+                                modifier = Modifier
+                                    .padding(padding.dp)
+                                    .background(MaterialTheme.colorScheme.primary, RoundedCornerShape(2.dp))
+                                    .size(
+                                        (width-padding).dp,
+                                        (height-padding).dp
+                                    )
+                            ){}
                         }
                    }
                }
