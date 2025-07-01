cb9e2b394c2761ab945b21806ba4064d47a92189
Uros Karaleic <karaleicuu@gmail.com>
2025-07-01 16:48:03 +0200

    buttons

--- src/main/kotlin/net/systemvi/configurator/components/configure/keyboard_layout/LayerSelector.kt
+++ src/main/kotlin/net/systemvi/configurator/components/configure/keyboard_layout/LayerSelector.kt
@@ -7,10 +7,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,6 +16,20 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import net.systemvi.configurator.components.configure.ConfigureViewModel
 import net.systemvi.configurator.components.configure.serial.SerialPortSelector
+
+@Composable fun UploadKeymapButton(){
+    OutlinedButton(
+        onClick = {}
+    ){
+
+    }
+}
+@Composable fun SaveAs(){
+
+}
+@Composable fun SaveButton(){
+
+}
 
 @Composable
 fun LayerSelector(configureViewModel: ConfigureViewModel = viewModel { ConfigureViewModel() }){
@@ -61,6 +72,11 @@ fun LayerSelector(configureViewModel: ConfigureViewModel = viewModel { Configure
                 }
             }
         }
-        SerialPortSelector()
+        Row{
+            UploadKeymapButton()
+            SaveButton()
+            SaveAs()
+            SerialPortSelector()
+        }
     }
 }
