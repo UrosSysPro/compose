3dd44896952a474fd14389a9ccd18e9401f8897c
Jovan Colovic <colovicjovan02@gmail.com>
2025-07-01 16:30:32 +0200

    remove grid1

--- src/main/kotlin/net/systemvi/configurator/components/tester/Grid.kt
+++ src/main/kotlin/net/systemvi/configurator/components/tester/Grid.kt
@@ -9,98 +9,117 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.input.key.*
 import androidx.compose.ui.layout.layoutId
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import arrow.core.getOrElse
 import arrow.core.toOption
 import net.systemvi.configurator.components.common.AutoSizingBox
+import net.systemvi.configurator.components.common.AutoSizingBoxItemPosition
 import net.systemvi.configurator.data.allKeys
+import net.systemvi.configurator.data.alphabetKeys
+import net.systemvi.configurator.model.Key
+import net.systemvi.configurator.model.KeyMap
+import net.systemvi.configurator.model.Keycap
 
-private data class GridItem(val value:String,val width:Float,val height:Float, val x:Int, val y:Int)
+@Composable fun Grid(keymap: KeyMap, keycapComponent: @Composable (String, Boolean, Boolean)->Unit, oneUSize:Int=50) {
+    data class GridItem(val keycap: Keycap, val x: Int, val y: Int)
 
-data class AutoSizingBoxItemPosition(val x: Dp, val y:Dp)
+    val viewModel = viewModel { TesterPageViewModel() }
 
-@Composable fun Grid(items:List<String>, keycap:@Composable (String, Boolean, Boolean)->Unit) {
-    val viewModel = viewModel{ TesterPageViewModel() }
-
-    val filteredItems = items.mapIndexed { j, row ->
-        row.split(" ").mapIndexed {i, it ->
-            val matches = "%(.*),([0-9]+.[0-9]+),([0-9]+.[0-9]+)".toRegex().find(it);
-            if (matches == null) {
-                GridItem(it, 1f, 1f, i, j)
-            } else {
-                val (name, width, height) = matches.destructured
-                GridItem(name.replace("\\s", " "), width.toFloat(), height.toFloat(), i, j)
-            }
+    val filteredItems = keymap.keycaps.mapIndexed { j, row ->
+        row.mapIndexed { i, keycap ->
+            GridItem(keycap, i, j)
         }
     }
-    val size = 50f
     var minSize = 1f
+    var maxPadding = 0f
 
-    val onKeyEvent:(KeyEvent) -> Boolean = {
-        if(it.type!= KeyEventType.Unknown){
-            if(it.type ==  KeyEventType.KeyDown){
-                println("key code: ${it.key.nativeKeyCode}")
-            }
-            val key = allKeys.find { key-> key.nativeCode.toInt() == it.key.nativeKeyCode }.toOption()
-            key.onSome { key->
-                when (it.type){
-                    KeyEventType.KeyDown -> {
-                        viewModel.currentlyDownKeys += key
-                        viewModel.wasDownKeys += key
-                        for(row in filteredItems){
-                            for(item in row){
-                                if(item.value.uppercase() == key.name.uppercase()){
-                                    viewModel.channels?.get(0)?.noteOn(item.x + item.y * 12, 93)
-                                }
-                            }
+    val passKey = alphabetKeys.last()
+
+    fun processEvent(type: KeyEventType, key: Key) {
+        println(key)
+        when (type) {
+            KeyEventType.KeyDown -> {
+                viewModel.currentlyDownKeys += key
+                viewModel.wasDownKeys += key
+                for (row in filteredItems) {
+                    for (item in row) {
+                        val currentKey = item.keycap.layers[0].getOrElse { passKey }
+                        if (currentKey.value == key.value) {
+                            viewModel.channels?.get(0)?.noteOn(item.x + item.y * 12, 93)
                         }
                     }
-                    KeyEventType.KeyUp -> {
-                        viewModel.currentlyDownKeys -= key
-                        for(row in filteredItems){
-                            for(item in row){
-                                if(item.value.uppercase() == key.name.uppercase()){
-                                    viewModel.channels?.get(0)?.noteOff(item.x + item.y * 12)
-                                }
-                            }
-                        }
-                    }
-
                 }
             }
-//            println("""
-//                |native code: ${it.key.nativeKeyCode}
-//                |key code: ${it.key.keyCode}
-//                |native location: ${it.key.nativeKeyLocation}
-//            """.trimIndent())
-            true
-        }else false
+
+            KeyEventType.KeyUp -> {
+                viewModel.currentlyDownKeys -= key
+                for (row in filteredItems) {
+                    for (item in row) {
+                        val currentKey = item.keycap.layers[0].getOrElse { passKey }
+                        if (currentKey.value == key.value) {
+                            viewModel.channels?.get(0)?.noteOff(item.x + item.y * 12)
+                        }
+                    }
+                }
+            }
+
+        }
     }
-    Box(Modifier
-        .onKeyEvent(onKeyEvent)
-        .focusRequester(viewModel.focusRequester)
-        .focusable()
+
+    val onKeyEvent: (KeyEvent) -> Boolean = { event ->
+        if (event.type == KeyEventType.Unknown) {
+            false
+        } else {
+            processEvent(
+                event.type,
+                allKeys.find { key -> key.nativeCode.toInt() == event.key.nativeKeyCode }.toOption().getOrElse {
+                    println(event.key.nativeKeyCode)
+                    passKey
+                }
+            )
+            true
+        }
+    }
+
+    Box(
+        Modifier
+            .onKeyEvent(onKeyEvent)
+            .focusRequester(viewModel.focusRequester)
+            .focusable()
     ) {
         AutoSizingBox {
             var currentX = 0f
             var currentY = 0f
             filteredItems.zip(filteredItems.indices).forEach { (row, j) ->
                 row.zip(row.indices).forEach { (item, i) ->
-                    minSize = minSize.coerceAtMost(item.height)
+                    val keycap = item.keycap
+                    val width = keycap.width.size
+                    val height = keycap.height.size
+                    val paddingLeft = keycap.padding.left
+                    val paddingBottom = keycap.padding.bottom
+                    val key = keycap.layers[0].getOrElse { passKey }
+                    minSize = minSize.coerceAtMost(height)
+                    maxPadding = maxPadding.coerceAtLeast(paddingBottom)
+                    currentX += oneUSize * paddingLeft
+
                     Box(
                         modifier = Modifier
                             .layoutId(AutoSizingBoxItemPosition(currentX.dp, currentY.dp))
-                            .size((size * item.width).dp, (size * item.height).dp)
+                            .size((oneUSize * width).dp, (oneUSize * height).dp)
                     ) {
-                        val key = allKeys.find{it.name.uppercase() == item.value.uppercase()}
-                        keycap(item.value, viewModel.wasDownKeys.contains(key), viewModel.currentlyDownKeys.contains(key))
-                        currentX += size * item.width
+                        keycapComponent(
+                            key.name,
+                            viewModel.wasDownKeys.contains(key),
+                            viewModel.currentlyDownKeys.contains(key)
+                        )
                     }
+                    currentX += oneUSize * width
                 }
                 currentX = 0f
-                currentY += minSize * size
+                currentY += (minSize + maxPadding) * oneUSize
                 minSize = 1f
+                maxPadding = 0f
             }
         }
         LaunchedEffect(Unit) {
