1ed867426c2c813bda927eeac5abc8b9e1f4b120
Uros Karaleic <karaleicuu@gmail.com>
2025-07-01 16:29:02 +0200

    use grid in configure page

--- src/main/kotlin/net/systemvi/configurator/components/configure/keyboard_layout/KeyboardLayoutView.kt
+++ src/main/kotlin/net/systemvi/configurator/components/configure/keyboard_layout/KeyboardLayoutView.kt
@@ -1,17 +1,25 @@
 package net.systemvi.configurator.components.configure.keyboard_layout
 
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.material3.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import net.systemvi.configurator.components.common.BorderHorizontal
 import net.systemvi.configurator.components.common.BorderVertical
 import net.systemvi.configurator.components.configure.ConfigureViewModel
 import net.systemvi.configurator.components.configure.KeyboardLayoutPages
+import net.systemvi.configurator.components.tester.Grid2
+import net.systemvi.configurator.components.tester.Keycap
 
 
 @Composable
@@ -26,7 +34,17 @@ fun KeyboardLayoutView() {
                 KeyboardLayoutPages.Keymap->Column{
                     LayerSelector()
                     BorderHorizontal()
-                    KeyboardLayoutGridView()
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier= Modifier.fillMaxWidth().weight(1f)
+                            .horizontalScroll(rememberScrollState())
+                            .verticalScroll(rememberScrollState())
+                    ) {
+                        if(viewModel.keymap!=null){
+                            Grid2(viewModel.keymap!!, Keycap)
+                        }
+                    }
                 }
                 KeyboardLayoutPages.SaveAndLoad-> SaveAndLoad()
             }
