f6e06b5a07e7a220c647bbfb3277b095cbac9c91
Jovan Colovic <colovicjovan02@gmail.com>
2025-07-08 17:40:03 +0200

    Added remove buttons to keycaps and remove row

--- src/main/kotlin/net/systemvi/configurator/components/design/DesignPage.kt
+++ src/main/kotlin/net/systemvi/configurator/components/design/DesignPage.kt
@@ -1,14 +1,24 @@
 package net.systemvi.configurator.components.design
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.unit.dp
 import arrow.core.right
 import arrow.optics.dsl.index
@@ -20,6 +30,7 @@ import net.systemvi.configurator.model.KeyMap
 import net.systemvi.configurator.model.Keycap
 import net.systemvi.configurator.model.keycaps
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable fun DesignPage(modifier: Modifier) {
     var keymap by remember { mutableStateOf(KeyMap("Untitled 1", listOf(listOf()))) }
 
@@ -74,9 +85,17 @@ import net.systemvi.configurator.model.keycaps
                     )
                 }
                 row.forEachIndexed { j, key ->
+                    var isHovered by remember { mutableStateOf(false) }
+
                     Box(
                         modifier = Modifier
                             .size(50.dp, 50.dp)
+                            .onPointerEvent(PointerEventType.Enter) {
+                                isHovered = true
+                            }
+                            .onPointerEvent(PointerEventType.Exit) {
+                                isHovered = false
+                            }
                     ) {
                         FlatKeycap(
                             KeycapParam(
@@ -84,7 +103,46 @@ import net.systemvi.configurator.model.keycaps
                                 KeycapPosition(0, 0)
                             )
                         )
+                        if (isHovered) {
+                            IconButton(
+                                onClick = {
+                                    keymap = KeyMap.keycaps.index(i).modify(keymap, {
+                                        it - key
+                                    })
+                                },
+                                modifier = Modifier
+                                    .align(Alignment.TopEnd)
+                                    .clip(CircleShape)
+                                    .size(20.dp)
+                                    .background(
+                                        color = MaterialTheme.colorScheme.error,
+                                    )
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Close,
+                                    contentDescription = "Remove",
+                                    tint = MaterialTheme.colorScheme.onError,
+                                    modifier = Modifier
+                                        .align(Alignment.Center)
+                                        .size(10.dp)
+                                )
+                            }
+                        }
                     }
+                }
+                ElevatedButton(
+                    onClick = {
+                        keymap = KeyMap.keycaps.modify(keymap, {keycaps ->
+                            keycaps.filterIndexed {index, row -> index != i}
+                        })
+                    },
+                    modifier = Modifier
+                        .padding(start = 10.dp)
+                ) {
+                    Text(
+                        "Remove Row",
+                        style = MaterialTheme.typography.bodySmall
+                    )
                 }
             }
         }
