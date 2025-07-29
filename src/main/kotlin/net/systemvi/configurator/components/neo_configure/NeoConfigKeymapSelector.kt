0ae04e8919e9d0b2725a22e68f7eafb3914a8878
Uros Karaleic <karaleicuu@gmail.com>
2025-07-29 16:13:06 +0200

    keymap selector radi animacija

--- src/main/kotlin/net/systemvi/configurator/components/neo_configure/NeoConfigKeymapSelector.kt
+++ src/main/kotlin/net/systemvi/configurator/components/neo_configure/NeoConfigKeymapSelector.kt
@@ -1,15 +1,133 @@
 package net.systemvi.configurator.components.neo_configure
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibilityScope
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionLayout
+import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalSharedTransitionApi::class)
 @Composable
 fun NeoConfigKeymapSelector(){
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+    SharedTransitionLayout{
+        AnimatedContent(
+            expanded,
+            label = "basic_transition"
+        ) { targetState ->
+            if (!targetState) {
+                ShowKeymapsButton(
+                    onExpand = {
+                        expanded = true
+                    },
+                    animatedVisibilityScope = this@AnimatedContent,
+                    sharedTransitionScope = this@SharedTransitionLayout
+                )
+            } else {
+                KeymapsPopUp(
+                    onCollapse = {
+                        expanded = false
+                    },
+                    animatedVisibilityScope = this@AnimatedContent,
+                    sharedTransitionScope = this@SharedTransitionLayout
+                )
+            }
+        }
+    }
+}
+@OptIn(ExperimentalSharedTransitionApi::class)
+@Composable
+private fun ShowKeymapsButton(
+    onExpand: () -> Unit,
+    sharedTransitionScope: SharedTransitionScope,
+    animatedVisibilityScope: AnimatedVisibilityScope
+) {
+    with(sharedTransitionScope) {
+        ElevatedButton(
+            onClick = onExpand,
+            modifier = Modifier
+                .sharedBounds(
+                    rememberSharedContentState(key = "container"),
+                    animatedVisibilityScope = animatedVisibilityScope
+                )
+        ){
+            Text(
+                text = "Keymaps",
+                modifier = Modifier
+                    .sharedElement(
+                        rememberSharedContentState(key = "title"),
+                        animatedVisibilityScope = animatedVisibilityScope
+                    )
+            )
+        }
+    }
+}
 
-    ElevatedButton(
-        onClick = {}
-    ){
-        Text("Keymaps")
+@OptIn(ExperimentalSharedTransitionApi::class)
+@Composable
+private fun KeymapsPopUp(
+    onCollapse: () -> Unit,
+    sharedTransitionScope: SharedTransitionScope,
+    animatedVisibilityScope: AnimatedVisibilityScope
+) {
+    with(sharedTransitionScope) {
+        Card(
+            modifier = Modifier
+                .sharedBounds(
+                    rememberSharedContentState(key = "container"),
+                    animatedVisibilityScope = animatedVisibilityScope
+                )
+        ) {
+            Column(
+                modifier = Modifier
+                    .clickable { onCollapse() }
+                    .size(400.dp,300.dp)
+                    .padding(top = 20.dp,),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    text = "Keymaps",
+                    modifier = Modifier
+                        .sharedElement(
+                            rememberSharedContentState(key = "title"),
+                            animatedVisibilityScope = animatedVisibilityScope
+                        ),
+                    fontWeight = FontWeight.Medium,
+                    color = MaterialTheme.colorScheme.primary
+                )
+                FlowRow(modifier = Modifier.weight(1f)){
+
+                }
+            }
+        }
     }
 }
